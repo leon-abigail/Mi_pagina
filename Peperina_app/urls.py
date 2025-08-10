2a50a3cb85cc25382d650a79be90ca51bea62d97
Abigail Leon <abili49.sabl@gmail.com>
2025-08-10 00:14:39 -0300

    Ahora ya funciona todo

--- Peperina_app/urls.py
+++ Peperina_app/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('productos/', views.lista_productos, name='lista_productos'),
     path('agregar-producto/', views.agregar_producto, name='agregar_producto'),
+    path("producto/<int:pk>/", views.detalle_producto, name="detalle_producto"),  # ← usa pk
+    path("agregar-producto/", views.agregar_producto, name="agregar_producto"),
     path('categorias/', views.lista_categorias, name='lista_categorias'),
-    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
+    
     
 ]
